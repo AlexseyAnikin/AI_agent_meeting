7b747e7fb03a6545ac4ba77ac7d828eeded3564b
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-08-26 14:03:46 +0300

    add: meeting_report

---
+++ models/meeting_report.py
@@ -0,0 +1,10 @@
+
+
+class MeetingReport:
+    def __init__(self, topic, summary, participants, decisions, tasks, open_questions):
+        self.topic = topic
+        self.summary = summary
+        self.participants = participants
+        self.decisions = decisions
+        self.tasks = tasks
+        self.open_questions = open_questions
