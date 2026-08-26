4482057d8a19e83351757b2adbefa0ecefd7062a
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-08-26 13:48:11 +0300

    add: добавление task

---
+++ models/task.py
@@ -0,0 +1,8 @@
+
+
+class Task: 
+    def __init__(self, description, assignee, deadline, priority):
+        self.description = description
+        self.assignee = assignee
+        self.deadline = deadline
+        self.priority = priority
