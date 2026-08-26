1d095116b11bc87271ff795ae7c52cb22a93cc41
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-08-26 18:24:56 +0300

    add: meeting_date добавление даты для того, чтобы были коректные сроки

--- models/meeting.py
+++ models/meeting.py
@@ -1,7 +1,8 @@
 class Meeting:
-    def __init__(self, audio_path):
+    def __init__(self, audio_path, meeting_date):
         self.audio_path = audio_path
         self.transcript = None
+        self.meeting_date = meeting_date
 
     def set_transcript(self, text):
         self.transcript = text
