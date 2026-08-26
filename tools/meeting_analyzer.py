e31df0119c29cc14d4539a0932e20ef352f89cae
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-08-26 17:56:17 +0300

    delete: test_ollama.py перенес функционал в meeting_analyzer.py

--- tools/meeting_analyzer.py
+++ tools/meeting_analyzer.py
@@ -1,8 +1,54 @@
+import json
+import ollama
+
 from models.task import Task
 from models.meeting_report import MeetingReport
 
 
 class MeetingAnalyzer:
+    def analyze(self, transcript):
+        response = ollama.chat(
+            model="qwen3:14b",
+            messages=[
+                {
+                    "role": "system",
+                    "content": (
+                        "Ты анализатор рабочих совещаний."
+                        "Твоя задача только извлекать факты из текста."
+                        "Не давать советов и не придумывай отсутствующую информацию."
+                        "Если значение неизвестно, верни только null."
+                        "Верни только JSON со следующими полями: "
+                        "topic, summary, participants, decisions, tasks, open_questions. "
+                        "participants, decisions, tasks и open_questions всегда должны быть массивами. "
+                        "Если элементов нет, возвращай пустой массив []. "
+                        "tasks должен быть массивом объектов с полями "
+                        "description, assignee, deadline, priority."
+                        "Если assignee, deadline или priority неизвестны, используй null."
+                    )
+                    
+                },
+                {
+                    "role": "user",
+                    "content": (
+                        "Добрый день. Начинаем рабочее совещание. "
+                        "Сегодня необходимо обсудить состояние проекта Meeting AI Agent. "
+                        "Алексей отвечает за разработку программы. "
+                        "До пятницы необходимо завершить модуль распознавания речи. "
+                        "Следующее совещание состоится на следующей неделе."
+                    )
+                }
+            ],
+            format="json",
+            options={
+                "temperature": 0
+            }
+        )
+
+        data = json.loads(response.message.content)
+
+        return self.create_report(data)
+        
+
     def create_report(self, data):
         tasks = []
 
